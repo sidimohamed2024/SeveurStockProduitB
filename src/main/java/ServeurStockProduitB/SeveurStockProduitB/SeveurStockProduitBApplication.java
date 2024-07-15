7227ee7b060b562243bb893f4d7f22b636ee9f1a
sidimohamed2024 <sidi.mohamed.noman@iscae.mr>
2024-07-15 08:46:19 +0300

    first commit du projet spring boot

---
+++ src/main/java/ServeurStockProduitB/SeveurStockProduitB/SeveurStockProduitBApplication.java
@@ -0,0 +1,13 @@
+package ServeurStockProduitB.SeveurStockProduitB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeveurStockProduitBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeveurStockProduitBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ServeurStockProduitB/SeveurStockProduitB/SeveurStockProduitBApplicationTests.java
@@ -0,0 +1,13 @@
+package ServeurStockProduitB.SeveurStockProduitB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeveurStockProduitBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
